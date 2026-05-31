84223a12b150b527c6cc6519d454ca262e654241
zenixeditz80-arch <zenixeditz80@gmail.com>
2026-05-31 12:01:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
-import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-# 🔐 SAFE TOKEN (Railway env)
-TOKEN = os.getenv("8514009693:AAEEdrOEV_8F8FpRsBT6fYWdlBMHenCcMjk")
-
+TOKEN = "8514009693:AAEEdrOEV_8F8FpRsBT6fYWdlBMHenCcMjk"
 ADMIN_ID = 8758830915
 
 bot = telebot.TeleBot(TOKEN)
 
-# temp storage
 user_data = {}
 
-# ---------------- START ----------------
 @bot.message_handler(commands=['start'])
 def start(message):
     if message.from_user.id != ADMIN_ID:
-        return bot.send_message(message.chat.id, "⛔ Admin only")
+        return
+    bot.send_message(message.chat.id, "📸 Send Photo")
 
-    bot.send_message(message.chat.id, "📸 Send Photo to start")
-
-# ---------------- CANCEL ----------------
 @bot.message_handler(commands=['cancel'])
 def cancel(message):
     if message.from_user.id != ADMIN_ID:
         return
-
     user_data.pop(message.chat.id, None)
     bot.send_message(message.chat.id, "❌ Cancelled")
 
-# ---------------- PHOTO ----------------
 @bot.message_handler(content_types=['photo'])
-def get_photo(message):
+def photo(message):
     if message.from_user.id != ADMIN_ID:
         return
 
+    user_data[message.chat.id] = {
+        "photo": message.photo[-1].file_id
+    }
+
+    bot.send_message(message.chat.id, "✍️ Send Message")
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    chat_id = message.chat.id
+
+    if chat_id not in user_data:
+        return
+
+    data = user_data[chat_id]
+
+    if "text" not in data:
+        data["text"] = message.text
+        bot.send_message(chat_id, "🔗 Send Link")
+        return
+
+    if "link" not in data:
+        data["link"] = message.text
+
+        markup = InlineKeyboardMarkup()
+        markup.add(
+            InlineKeyboardButton("ကြည့်ရန်", url=data["link"])
+        )
+
+        bot.send_photo(
+            chat_id,
+            data["photo"],
+            caption=data["text"],
+            reply_markup=markup
+        )
+
+        user_data.pop(chat_id, None)
+
+print("Bot Running...")
+bot.infinity_polling()
     user_data[message.chat.id] = {
         "photo": message.photo[-1].file_id
     }
